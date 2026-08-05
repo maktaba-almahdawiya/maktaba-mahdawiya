a498cb2ce241ad51c121dd0f8921e1283d0f8a6f
maktaba-almahdawiya <a3mhtmal@gmail.com>
2026-08-05 16:13:46 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,51 @@
+async function loadBooks() {
 
+const container = document.getElementById("booksContainer");
+
+if(!container) return;
+
+const response = await fetch("data/books.json");
+
+const books = await response.json();
+
+container.innerHTML = "";
+
+books.forEach(book=>{
+
+container.innerHTML += `
+
+<div class="book-card">
+
+<img src="${book.cover}" alt="${book.title}">
+
+<div class="book-info">
+
+<div class="book-title">
+
+${book.title}
+
+</div>
+
+<div class="book-author">
+
+${book.author}
+
+</div>
+
+<a class="read-btn" href="reader.html?book=${book.id}">
+
+قراءة الكتاب
+
+</a>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+loadBooks();
